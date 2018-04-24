25beda77e15dfdfbdab92a4de63fda8f2f5325b3
Dumdumdurum <mlenik1990@gmail.com>
2018-04-24 05:36:50 +0200

    0.7.0 slider still broken

--- js/app.js
+++ js/app.js
@@ -18,17 +18,93 @@ function playFunction(){
     vid.style.display = 'block';
     document.getElementById('playBtn').style.display = 'none';
     vid.play();
+    vid.onpause = function(){
+        vid.style.display = 'none';
+        document.getElementById('playBtn').style.display = 'block';
+    };
 }
-var vid = document.getElementById('videoStory');
 
-document.getElementById('videoStory').onpause = function(){
-    document.getElementById('videoStory').style.display = 'none';
-    document.getElementById('playBtn').style.display = 'block';
-    alert("video paused")
-};
+
 
 function pauseFunction(){
     document.getElementById('videoStory').style.display = 'none';
     document.getElementById('playBtn').style.display = 'block';
 }
+$(function() {
+    console.log('DOM1');
+    $(document).ready(function () {
+        //rotation speed and timer
+        console.log('DOM');
+        var speed = 500;
 
+        var run = setInterval(rotate, speed);
+        var slides = $('.slide');
+        var container = $('#slides ul');
+        var elm = container.find(':first-child').prop("tagName");
+        var item_width = container.width();
+        var prev = 'prev'; //id of previous button
+        var next = 'next'; //id of next button
+        slides.width(item_width); //set the slides to the correct pixel width
+        container.parent().width(item_width);
+        container.width(slides.length * item_width); //set the slides container to the correct total width
+        container.find(elm + ':first').before(container.find(elm + ':last'));
+        resetSlides();
+
+
+        //if user clicked on prev button
+
+        $('#buttons a').click(function (e) {
+            //slide the item
+            console.log('click');
+
+            if (container.is(':animated')) {
+                return false;
+            }
+            if (e.target.id === prev) {
+                console.log('prev');
+                container.stop().animate({
+                    'left': 0
+                }, 1500, function () {
+                    container.find(elm + ':first').before(container.find(elm + ':last'));
+                    resetSlides();
+                });
+            }
+
+            if (e.target.id === next) {
+                console.log('next');
+                container.stop().animate({
+                    'left': item_width * -2
+                }, 1500, function () {
+                    container.find(elm + ':last').after(container.find(elm + ':first'));
+                    resetSlides();
+                });
+            }
+
+            //cancel the link behavior
+            return false;
+
+        });
+
+        //if mouse hover, pause the auto rotation, otherwise rotate it
+        container.parent().mouseenter(function () {
+            clearInterval(run);
+        }).mouseleave(function () {
+            run = setInterval(rotate, speed);
+        });
+
+
+        function resetSlides() {
+            //and adjust the container so current is in the frame
+            container.css({
+                'left': -1 * item_width
+            });
+        }
+
+    });
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin
+
+    function rotate() {
+        $('#next').click();
+    }
+});
